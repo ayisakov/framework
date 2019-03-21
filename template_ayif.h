d0b35797898529b6628cf28aad68e95d7959c26c
Artem Isakov <artjom.isakov@gmail.com>
2019-03-21 11:59:32 -0400

    Added convenience template.

---
+++ template_ayif.h
@@ -0,0 +1,18 @@
+#ifndef NAME_H
+#define NAME_H
+
+
+namespace ayisakov
+{
+namespace framework
+{
+class Name
+{
+  public:
+    Name();
+    virtual void ~Name();
+};
+} // namespace framework
+} // namespace ayisakov
+
+#endif // NAME_H
