82f0ed417abe2d62bfdefd489c7fa5031537639c
Artem Isakov <artjom.isakov@gmail.com>
2019-03-21 11:58:38 -0400

    Added json submodule and modified the base config interface.

---
+++ IJSONConvertible.h
@@ -0,0 +1,32 @@
+#ifndef IJSONCONVERTIBLE_H
+#define IJSONCONVERTIBLE_H
+
+#include <string>
+
+namespace ayisakov
+{
+namespace framework
+{
+class IJSONConvertible
+{
+  public:
+    IJSONConvertible() {}
+
+    virtual void ~IJSONConvertible() {}
+
+    /**
+     * Load object state from JSON string
+     *
+     * @throw std::runtime_error if unable to parse
+     */
+    virtual void fromJSON(const std::string& json) = 0;
+
+    /**
+     * Export state as JSON string
+     */
+    virtual std::string toJSON() = 0;
+};
+} // namespace framework
+} // namespace ayisakov
+
+#endif // IJSONCONVERTIBLE_H
